85dcbc4b5b43b85fc4a850fd518a0e1aa1022f4d
MaxRev <cmthea32@gmail.com>
2021-04-20 03:17:31 +0300

    code cleanup

--- editor/Extensions/AssemblyExtensions.cs
+++ editor/Extensions/AssemblyExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace EditorProject
+namespace EditorProject.Extensions
 {
     internal class AssemblyExtensions
     {
